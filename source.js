dac8c892da55cb823d522eda30bad01576d590bb
Robert King <robert.o.king@hotmail.co.uk>
2021-04-09 01:15:13 +0200

    Added: Basic handleCSSInput() functionality - CSS entered by the user automatically updates displayed result

--- source.js
+++ source.js
@@ -1,5 +1,9 @@
 const handleCSSInput = () => {
   const html = '<p>Test!</p>';
-  const css = document.getElementById('css').value;
-  console.log(css);
+  const css = `<style>${document.getElementById('css').value}</style>`;
+
+  const outputUser = document.getElementById('output-user').contentWindow.document;
+  outputUser.open();
+  outputUser.write(html + css);
+  outputUser.close();
 };
