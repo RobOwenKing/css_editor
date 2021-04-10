8249c13806b9352b7037ff2a53db7464a3a2890a
Robert King <robert.o.king@hotmail.co.uk>
2021-04-11 00:50:13 +0200

    Added: JS to fill output element on textarea input

--- source.js
+++ source.js
@@ -1,8 +1,16 @@
+const cssInput = document.getElementById('css');
+const cssDisplay = document.getElementById('highlighted-css');
+const outputUser = document.getElementById('output-user').contentWindow.document;
+
+// Initialise the highlighted output with whatever is in the input
+cssDisplay.textContent = cssInput.value;
+
 const handleCSSInput = () => {
+  cssDisplay.textContent = cssInput.value;
+
   const html = '<p>Test!</p>';
   const css = `<style>${document.getElementById('css').value}</style>`;
 
-  const outputUser = document.getElementById('output-user').contentWindow.document;
   outputUser.open();
   outputUser.write(html + css);
   outputUser.close();
