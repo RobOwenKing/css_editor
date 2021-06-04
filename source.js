a8914ac16aa8602798d541d7642645cedd2d9a72
Robert King <robert.o.king@hotmail.co.uk>
2021-06-04 19:53:05 +0200

    Added: outputTarget and tabs for outputs

--- source.js
+++ source.js
@@ -3,7 +3,12 @@ const showCSS     = document.getElementById('show-css');
 const htmlDisplay = document.getElementById('html');
 const cssInput    = document.getElementById('css');
 const cssDisplay  = document.getElementById('highlighted-css');
-const outputUser  = document.getElementById('output-user').contentWindow.document;
+
+const showOutputTarget = document.getElementById('show-target');
+const showOutputUser   = document.getElementById('show-user');
+const outputTarget     = document.getElementById('output-target')
+const outputUser       = document.getElementById('output-user')
+const outputUserDoc    = outputUser.contentWindow.document;
 
 const html = '<p>Test!</p>';
 
@@ -30,6 +35,27 @@ showHTML.addEventListener('keydown', showHTMLClick);
 showCSS.addEventListener('click', showCSSClick);
 showCSS.addEventListener('keydown', showCSSClick);
 
+const showOutputTargetClick = (event) => {
+  outputTarget.style.display = 'block';
+  outputUser.style.display = 'none';
+
+  showOutputTarget.classList.add('tab-active');
+  showOutputUser.classList.remove('tab-active');
+};
+
+const showOutputUserClick = (event) => {
+  outputTarget.style.display = 'none';
+  outputUser.style.display = 'block';
+
+  showOutputTarget.classList.remove('tab-active');
+  showOutputUser.classList.add('tab-active');
+};
+
+showOutputTarget.addEventListener('click', showOutputTargetClick);
+showOutputTarget.addEventListener('keydown', showOutputTargetClick);
+showOutputUser.addEventListener('click', showOutputUserClick);
+showOutputUser.addEventListener('keydown', showOutputUserClick);
+
 // Initialise the htmlDisplay
 htmlDisplay.innerText = html;
 
@@ -38,7 +64,7 @@ const handleCSSInput = () => {
 
   const css = `<style>${cssInput.value}</style>`;
 
-  outputUser.open();
-  outputUser.write(html + css);
-  outputUser.close();
+  outputUserDoc.open();
+  outputUserDoc.write(html + css);
+  outputUserDoc.close();
 };
