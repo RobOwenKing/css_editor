9d1b8171752547ddf7237fb3aa43a187aee5be03
Robert King <robert.o.king@hotmail.co.uk>
2021-07-14 20:00:25 +0200

    Added: Challenge Test 1

--- js/challenges.js
+++ js/challenges.js
@@ -1,3 +1,9 @@
-const CHALLENGES = {
-
-};
+const CHALLENGES = [
+  {
+    optgroup: "Tests",
+    name: "Test 1",
+    id: 1,
+    html: "<p>Test 1</p>",
+    targetCSS: "p {\ncolor: white;\n}"
+  }
+];
